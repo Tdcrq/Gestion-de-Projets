51d6099e9d568a0882489771ab3f98497cf0f969
tducrocq <tducrocq2612@gmail.com>
2022-10-04 16:37:52 +0200

    front skeleton

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <!-- ICON -->
+        <script src="https://kit.fontawesome.com/96626c594d.js" crossorigin="anonymous"></script>
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+
+    <body>
+        <nav>
+            <div class="div-logo-nav">
+                <img class="logo" src="logo-mentalworks-blanc.png" alt="ERROR">
+            </div>
+            <i class="nav-icon fa-solid fa-bars"></i>
+        </nav>
+
+        <main>
+            <section class="left-section"></section>
+            <section class="right-section"></section>
+        </main>
+    </body>
+
+    <footer>
+    </footer>
+</html>
