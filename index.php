09123d625301ba19868ea09d9c8513b349b53996
tducrocq <tducrocq2612@gmail.com>
2022-10-06 16:53:07 +0200

    create style

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -11,19 +7,49 @@
         <!-- ICON -->
         <script src="https://kit.fontawesome.com/96626c594d.js" crossorigin="anonymous"></script>
         <!-- CSS -->
+        <link rel="stylesheet" href="css/colors.css">
         <link rel="stylesheet" href="css/index.css">
     </head>
 
     <body>
         <nav>
-            <div class="div-logo-nav">
-                <img class="logo" src="logo-mentalworks-blanc.png" alt="ERROR">
-            </div>
+            <img class="logo" src="image/logo-mentalworks-blanc.png" alt="ERROR">
             <i class="nav-icon fa-solid fa-bars"></i>
         </nav>
 
         <main>
-            <section class="left-section"></section>
+            <section class="left-section">
+                <div class="layouts">
+                    <div class="layout dashboard">
+                        <i class="fa-sharp fa-solid fa-house-chimney"></i>
+                        TABLEAU DE BORD
+                    </div>
+
+                    <div class="separator"></div>
+
+                    <div class="layout">
+                        <i class="fa-solid fa-circle-user"></i>
+                        PROJETS
+                    </div>
+
+                    <div class="separator"></div>
+
+                    <div class="layout">
+                        <i class="fa-solid fa-book"></i>
+                        CLIENTS
+                    </div>
+
+                    <div class="separator"></div>
+
+                    <div class="layout">
+                        <i class="fa-regular fa-square-check"></i>
+                        HÉBERGEURS
+                    </div>
+
+                    <div class="separator"></div>
+                </div>
+            </section>
+
             <section class="right-section"></section>
         </main>
     </body>
