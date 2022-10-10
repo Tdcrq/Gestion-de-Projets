7cdab566737cade58feaaa1f6f5c094afeb1e51c
Loris Daca <losko@MacBook-Pro-de-Loris.local>
2022-10-10 13:54:27 +0200

    add autoloader

---
+++ require/autoloader.php
@@ -0,0 +1,24 @@
+<?php
+
+class Autoloader{
+
+    public static function register(){
+        spl_autoload_register(function ($class){
+            $file = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+            $fileArray = explode(DIRECTORY_SEPARATOR, $file);
+            if($fileArray[0] == "App"){
+                $fileArray[0] = "SRC";
+            }
+            //$file = implode(','$fileArray);
+            $file = implode(DIRECTORY_SEPARATOR, $fileArray);
+            var_dump($file);
+            if(file_exists($file)) {
+                require $file;
+                return true;
+            }
+            return false;
+        });
+    }
+}
+
+Autoloader::register();
