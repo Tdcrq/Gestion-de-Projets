245f5fb5d10fdb142024e063ef3da8becb373ed1
hugo <45632541+hugo2345@users.noreply.github.com>
2022-10-15 19:21:37 +0200

    new class connexion

---
+++ src/class/DB/ConnexionBdd.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Class\DB;
+use PDO;
+use PDOException;
+
+class ConnexionBdd
+{
+    public function __construct(
+        private string $server = "localhost",
+        private string $user = "root",
+        private string $pass = "root",
+        private string $dbName = "gestionprojts"
+        )
+        {
+        }
+
+    public function co()
+    {
+        try
+        {
+            $attributs = $this->getAttributs();
+            $co = new PDO("mysql:host=" . $attributs[0] .";dbname=" . $attributs[3] . ";charset=utf8", $attributs[1], $attributs[2]);
+            $co->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $e)
+        {
+            die('Erreur : ' . $e->getMessage());
+        }
+        return $co;
+    }
+    public function getAttributs(): array
+    {
+        return array($this->server, $this->user, $this->pass, $this->dbName);
+    }
+}
