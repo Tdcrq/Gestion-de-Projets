21af378970e8beb0fc0ab064ceb97c3a2f89013a
Loris Daca <losko@MacBook-Pro-de-Loris.local>
2022-11-06 18:47:15 +0100

    Fix cookie Reload default.

--- Container/Customer/View.php
+++ Container/Customer/View.php
@@ -1,7 +1,10 @@
 <?php
-
-if ($route[1] === "add") {
-    require("Container/Customer/Insert.php");
-} elseif ($route[1] === "modify") {
-    require("Container/Customer/Update.php");
-}
+if (!isset($route[1])) {
+    require("Container/Dashboard/index.php");
+} else{
+    if ($route[1] === "add") {
+        require("Container/Customer/Insert.php");
+    } elseif ($route[1] === "modify") {
+        require("Container/Customer/Update.php");
+    }
+} 
--- Container/Host/View.php
+++ Container/Host/View.php
@@ -1,7 +1,10 @@
 <?php
-
-if ($route[1] === "add") {
-    require("Container/Host/Insert.php");
-} elseif ($route[1] === "modify") {
-    require("Container/Host/Update.php");
+if (!isset($route[1])) {
+    require("Container/Dashboard/index.php");
+} else {
+    if ($route[1] === "add") {
+        require("Container/Host/Insert.php");
+    } elseif ($route[1] === "modify") {
+        require("Container/Host/Update.php");
+    }
 }
