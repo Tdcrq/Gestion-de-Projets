5d2a176cb411769e02cf315ba2d60131a9d1a19f
Loris Daca <losko@MacBook-Pro-de-Loris.local>
2022-11-06 14:57:16 +0100

    Début Php delete

--- Container/CommonDelete/delete.php
+++ Container/CommonDelete/delete.php
@@ -1,1 +1,21 @@
-<?php 
+<?php
+
+require_once("../../vendor/autoload.php");
+use App\DB\ConnexionBdd;
+
+$config = new ConnexionBdd();
+$co = $config->co();
+$id = $_GET["id"];
+$route = $_GET["route"];
+
+$route == "cli" ? $route = "customer" : $route = "host";
+
+
+$query = $co->prepare("SELECT * FROM ? WHERE id = ?");
+$query->bindParam('1', $route, PDO::PARAM_STR);
+$query->bindParam('2', $id, PDO::PARAM_INT);
+var_dump($query);
+$query->execute();
+$theCustomer = $query->fetchAll();
+var_dump($theCustomer);
+
